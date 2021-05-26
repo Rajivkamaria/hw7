5676c1a4746735c9a967d6a4ece01233c976d732
Raj Kamaria <raj.kamaria@kellogg.northwestern.edu>
2021-05-26 14:48:54 -0500

    ok

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,12 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {apiKey: "AIzaSyAI_lob1DNfnq87uepNUBOaWtkLTb1rpf0",
+authDomain: "kiei-451-2e14b.firebaseapp.com",
+projectId: "kiei-451-2e14b",
+storageBucket: "kiei-451-2e14b.appspot.com",
+messagingSenderId: "252814257625",
+appId: "1:252814257625:web:9075da8bce15c705307dc8"} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
